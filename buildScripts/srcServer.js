51956f22570ffeadcd93070c413087028a6bdd2c
Prince Odia <princeodia@gmail.com>
2022-03-09 11:16:40 +0100

    Transpile build scripts with babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
 const port = process.env.PORT || 3000;
 const app = express();
