409d953233536c66320b2d7294a0aaabc297b49e
Prince Odia <princeodia@gmail.com>
2022-03-11 09:25:07 +0100

    Configure webpack with express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import config  from '../webpack.config.dev';
+import webpack from 'webpack';
 
 const port = process.env.PORT || 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
