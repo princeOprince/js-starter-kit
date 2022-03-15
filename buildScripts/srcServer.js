8117d1d1616084dc9c8b778cf19ad977d75d8691
Prince Odia <princeodia@gmail.com>
2022-03-15 09:28:25 +0100

    Hardcode json user database

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {
+      "id": 1,
+      "firstname": "Bob",
+      "lastName": "Smith",
+      "email": "bob@gmail.com"
+    },
+    {
+      "id": 2,
+      "firstname": "Tammy",
+      "lastName": "Norton",
+      "email": "tnorton@yahoo.com"
+    },
+    {
+      "id": 3,
+      "firstname": "Tina",
+      "lastName": "Lee",
+      "email": "lee.tina@hotmail.com"
+    }
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
