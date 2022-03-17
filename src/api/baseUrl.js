d173183e75b2ae17e58b4bed3e3758d930626964
Prince Odia <princeodia@gmail.com>
2022-03-17 12:39:05 +0100

    Toggle mock api

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,13 @@
 export default function getBaseUrl() {
-  const inDevelopment = location.hostname === "localhost";
-  return inDevelopment ? "http://localhost:4001/" : "/";
+  return getQueryStringParameterByName('useMockApi') ? "http://localhost:4001/" : "/";
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[[\]]/g, '\\$&');
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
